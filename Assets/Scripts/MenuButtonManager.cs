85a86bcd7790222ec9b0b0a161ae46ccecea2647
Kevin Escobar Rodriguez <kevinaer@mit.edu>
2016-05-06 04:52:29 -0400

    Added a basic start screen template

---
+++ Assets/Scripts/MenuButtonManager.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class MenuButtonManager : MonoBehaviour {
+	public void StartGame(){
+		SceneManager.LoadScene (1);
+	}
+	public void QuitGame(){
+		Application.Quit ();
+	}
+}
